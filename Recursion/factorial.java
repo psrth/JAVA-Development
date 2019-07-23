30c2e1447ef3ee9498a85b0884073330e170eccc
Parth Sharma <parthsharma151@gmail.com>
2019-07-23 14:42:18 +0530

    recursion/factorial

---
+++ Recursion/factorial.java
@@ -0,0 +1,35 @@
+// Program to calculate factorial of an input recursively
+// Version: 1.0.2
+// Date: 23.7.2019
+// Author: Parth Sharma
+
+import java.util.Scanner;
+
+public class Factorial
+{
+
+
+  public static int fact(int n)
+  {
+      if (n>=1)
+        return n*fact(n-1);
+
+      else
+        return 1;
+  }
+
+
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the number: ");
+    int i = sc.nextInt();
+
+    int f = fact(input);
+
+    System.out.println("The factorial of "+i+" is: "+f);
+  }
+
+
+}
