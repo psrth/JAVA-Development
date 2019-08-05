5a102d1ab21d0039ef7db0ae324ba85acfad8af9
Parth Sharma <parthsharma151@gmail.com>
2019-08-05 09:39:11 +0530

    Update FiboSeries.java

--- Recursion/FiboSeries.java
+++ Recursion/FiboSeries.java
@@ -3,35 +3,14 @@
 // Date: 23.7.2019
 // Author: Parth Sharma
 
+// 0 1 1 2 3 5 8 13 21 34 55
+
 import java.util.Scanner;
 
 public class FiboSeries
 {
+  public static void printFibo(int n)
+  {
 
-  int n1 = 0;
-  int n2 = 1;
-  int n3 = 0;
-
-    public static void main(String args[])
-    {
-
-      Scanner sc = new Scanner(System.in);
-
-      System.out.print("Enter number of terms: ");
-      int num = sc.nextInt();
-
-      System.out.print(n1+" "+n2);
-      printFibo(num-2);
-
-    }
-
-    public static void printFibo(int n)
-    {
-      n3 = n1 + n2;
-      n1 = n2;
-      n2 = n3;
-
-      System.out.print(" "+n3);
-      printFibo(n-1);
-    }
+  }
 }
